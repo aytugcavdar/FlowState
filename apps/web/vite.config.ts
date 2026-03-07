eb829bec06f1c4a6b7b47c9bb1756c74764f13a6
aytug cavdar <aytugcavdar@gmail.com>
2026-03-07 05:50:18 +0300

    fix base url

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FlowState/',
+  base: './',
   resolve: {
     alias: {
       // FSD yapısı için @ alias
